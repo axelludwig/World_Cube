6fb7d308a1e7696313733a2b0715aefc92389942
Guillaume riedinger <guillaume.riedinger@h4d.com>
2022-05-07 19:46:38 +0200

    Cacaca

--- Assets/Scripts/GameNetworkManager.cs
+++ Assets/Scripts/GameNetworkManager.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UNET;
 
 public class GameNetworkManager : MonoBehaviour
 {
+    public static string ipAddress;
     void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        GUILayout.BeginArea(new Rect(10, 10, 300, 400));
+
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
+            ipAddress = GUILayout.TextField(ipAddress);
             StartButtons();
         }
         else
@@ -24,9 +28,16 @@ public class GameNetworkManager : MonoBehaviour
 
     static void StartButtons()
     {
+        NetworkManager.Singleton.gameObject.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
         if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+
         if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+
+        if (GUILayout.Button("Client"))
+        {
+            NetworkManager.Singleton.gameObject.GetComponent<UNetTransport>().ConnectAddress = ipAddress;
+            NetworkManager.Singleton.StartClient();
+        }
     }
 
     static void StatusLabels()
@@ -37,6 +48,7 @@ public class GameNetworkManager : MonoBehaviour
         GUILayout.Label("Transport: " +
             NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
         GUILayout.Label("Mode: " + mode);
+        GUILayout.Label("Connected to ip : " + NetworkManager.Singleton.gameObject.GetComponent<UNetTransport>().ConnectAddress);
     }
 
     static void SubmitNewPosition()
